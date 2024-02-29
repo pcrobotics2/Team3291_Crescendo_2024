d9815a706c6b0bd87fa9a831325ef9dd77f8d81d
PCRobotics <pcrobotoics@apps.district279.org>
2024-02-28 20:33:21 -0600

    Added pivot motor folder and tidied up IntakeSubsystem

--- src/main/java/frc/robot/commands/IntakeCMDS/PivotMotor/StowCMD.java
+++ src/main/java/frc/robot/commands/IntakeCMDS/PivotMotor/StowCMD.java
@@ -2,33 +2,31 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands.IntakeCMDS;
+package frc.robot.commands.IntakeCMDS.PivotMotor;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants;
+import frc.robot.subsystems.ColorChanger;
 import frc.robot.subsystems.IntakeSubsystem;
-import frc.robot.subsystems.IntakeSubsystem.PivotTarget;
 
-public class AmpCMD extends Command {
-  /** Creates a new AmpCMD. */
+public class StowCMD extends Command {
+  /** Creates a new StowCMD. */
   IntakeSubsystem intakeSubsystem;
-  public AmpCMD(IntakeSubsystem intakeSubsystem) {
+  public StowCMD(IntakeSubsystem intakeSubsystem) {
     // Use addRequirements() here to declare subsystem dependencies.
     this.intakeSubsystem = intakeSubsystem;
     addRequirements(intakeSubsystem);
   }
-
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+     
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    //intakeSubsystem.goToAmp();
-    intakeSubsystem.goToAmp();
-
+    intakeSubsystem.goToStow();
 
   }
 
@@ -36,7 +34,6 @@ public class AmpCMD extends Command {
   @Override
   public void end(boolean interrupted) {
     intakeSubsystem.stopIntake();
-    
   }
 
   // Returns true when the command should end.
