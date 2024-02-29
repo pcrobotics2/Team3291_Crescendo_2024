8c83decae6b2775e9fd4353154f7fd4507cea185
PC Robotics <pcrobotics2@apps.district279.org>
2024-02-29 15:57:54 -0600

    Made an auto that does mild (TJ yes)

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -96,8 +96,8 @@ public class SwerveSubsystem extends SubsystemBase {
             this::getSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
             this::driveRobotRelative, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
             new HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should likely live in your Constants class
-                    new PIDConstants(0.01, 0.0, 0.0), // Translation PID constants
-                    new PIDConstants(0.01, 0.0, 0.0), // Rotation PID constants
+                    new PIDConstants(5.01, 0.0, 0.0), // Translation PID constants
+                    new PIDConstants(5.01, 0.0, 0.0), // Rotation PID constants
                     Constants.Swerve.maxSpeed, // Max module speed, in m/s
                     0.38166088514508, // Drive base radius in meters. Distance from robot center to furthest module.
                     new ReplanningConfig() // Default path replanning config. See the API for the options here
